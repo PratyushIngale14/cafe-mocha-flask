5f69d618d70b87acd135c37f56b715a83129d3cc
Pratyush Anil Ingale <pratyushingale1401@gmail.com>
2025-03-27 16:52:34 -0500

    Update app.py

--- app.py
+++ app.py
@@ -25,5 +25,6 @@ if __name__ == "__main__":
     
 if __name__ == "__main__":
     import os
-    port = int(os.environ.get("PORT", 5000))
-    app.run(debug=False, host='0.0.0.0', port=port)
+    port = int(os.environ.get("PORT", 10000)) 
+    app.run(host="0.0.0.0", port=port)
+
